aedabaa117458ba5a967d36f30de2f4e4101cac9
y-qeen <okayqeenkhal@gmail.com>
2026-06-10 10:43:56 +0800

    Set up of login

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,25 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+ import {
+   DarkTheme,
+   DefaultTheme,
+   ThemeProvider,
+ } from "@react-navigation/native";
+ import { Stack } from "expo-router";
+ import { StatusBar } from "expo-status-bar";
+ import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/use-color-scheme';
+ import { useColorScheme } from "@/hooks/use-color-scheme";
+ 
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+ export default function RootLayout() {
+   const colorScheme = useColorScheme();
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
-  );
-}
+   return (
+     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+       <Stack initialRouteName="login">
+         <Stack.Screen name="login" />
+         <Stack.Screen name="(tabs)" options= {{headerShown : false}}/>
+       </Stack>
+       <StatusBar style="auto" />
+     </ThemeProvider>
+   );
+ }
